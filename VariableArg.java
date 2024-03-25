c72cddd28ba804877497ba1b9f02d7172ad40b20
Ninja-en <120076223+Ninja-en@users.noreply.github.com>
2024-03-25 22:27:29 +0700

    Add files via upload

---
+++ VariableArg.java
@@ -0,0 +1,12 @@
+public class VariableArg {
+    public static void main(String[] args) {
+        summation(10,20,30);
+    }
+    public static void summation(int...number){
+        int sum=0;
+        for(int i=0;i<number.length;i++){
+            sum+=number[i];
+        }
+        System.out.println("ผลรวม = "+sum);
+    }
+}
